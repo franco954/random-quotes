333bf71f3031e0b209479b15c794872c9be63ab9
franco954 <franconunez500@gmail.com>
2023-03-05 01:44:37 -0300

    first commit

---
+++ src/components/card.js
@@ -0,0 +1,62 @@
+
+import { useState, useEffect } from "react";
+
+
+export default function Card() {
+
+    const [content, setContent] = useState()
+    const [author, setAuthor] = useState()
+
+    const [loading, setLoading] = useState(true)
+
+
+    useEffect(() => {
+        requestQuote()
+    },[]);
+
+    const requestQuote = () => {
+        fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => {
+        const { content, author } = data; 
+        setContent(content);
+        setAuthor(author);
+        setLoading(false)
+      })
+      .catch((error) => console.error(error));
+    }
+
+
+  const handleOnClick = (e) => {
+    e.preventDefault();
+    setLoading(true)
+    requestQuote()
+  };
+
+  return (
+
+
+    loading ? <div class="spinner-grow text-info" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div> :
+    <div class="card text-white mb-3 p-4" id="quote-box">
+      <div class="card-body" >
+        <h5 class="card-text" id="text">
+            {content}
+        </h5>
+        <h6 class="card-subtitle" id="author"> - {author} </h6>
+      </div>
+      <div className="card-footer">
+        <div>
+          <button onClick={handleOnClick} id="new-quote" type="button" class="btn btn-info">
+            {" "}
+            New Quote
+          </button>
+        </div>
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+  <a href="https://twitter.com/intent/tweet" target="_blank" id="tweet-quote">Share on Twitter</a>
+</div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/main.js
@@ -0,0 +1,25 @@
+import Card from "../components/card";
+
+
+
+export default function Main() {
+    
+
+    return(
+
+        <div className="m-0 vh-100 row justify-content-center align-items-center">
+            <div  className="col-auto p5-text-center">
+            <Card/>
+            </div>
+        </div>
+
+
+
+            
+     
+
+    )
+
+
+
+}
